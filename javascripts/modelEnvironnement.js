7a1d7598bda3746003a596cc47eba8ef25eec729
Spefire <nicholasb@hotmail.fr>
2017-10-25 13:57:11 +0200

    feat(vegetation): add trees by random

--- javascripts/modelEnvironnement.js
+++ javascripts/modelEnvironnement.js
@@ -5,7 +5,17 @@
 // ------------------------------- //
 
 function createEnvironnement(epaisseurMur, hauteurMur, epaisseurSol, detailsTexture, scene){
+	
 	createGround(0,-hauteurMur/2,0,100,100);
+	
+	//Tree 01
+	var heightT01 = 6;
+	var nbT01 = 20;
+	for (var i = 0; i < nbT01; i++) {
+		var x = getRandomInt(-20, 20);
+		var z = getRandomInt(-25, -20);
+		createTree(x,0+heightT01/4,z,heightT01/2,heightT01);
+	}
 	return scene;
 }
 
@@ -19,4 +29,23 @@ function createGround(x, y, z, width, height, scene) {
 	mat.diffuseTexture.uScale = width/detailsTexture;
 	mat.diffuseTexture.vScale = width/detailsTexture;
 	ground.material = mat;
+}
+
+function createTree(x, y, z, width, height, scene) {
+
+	var mat = new BABYLON.StandardMaterial("tree_mat",scene);
+	mat.diffuseTexture = new BABYLON.Texture("assets/vegetation/arbre01.png",scene);
+	mat.diffuseTexture.uScale = 1.0;
+	mat.diffuseTexture.vScale = 1.0;
+	mat.diffuseTexture.hasAlpha = true;
+
+	var tree01 = BABYLON.MeshBuilder.CreatePlane("tree01", {width:width, height:height, sideOrientation:BABYLON.Mesh.DOUBLESIDE}, scene);
+	tree01.position = new BABYLON.Vector3(x,y,z) ;
+	tree01.material = mat;
+	setRotation(tree01,0,45,0);
+	
+	var tree02 = BABYLON.MeshBuilder.CreatePlane("tree02", {width:width, height:height, sideOrientation:BABYLON.Mesh.DOUBLESIDE}, scene);
+	tree02.position = new BABYLON.Vector3(x,y,z) ;
+	tree02.material = mat;
+	setRotation(tree02,0,-45,0);
 }
--- javascripts/utils.js
+++ javascripts/utils.js
@@ -10,3 +10,8 @@ function setRotation(obj, angleX, angleY, angleZ){
 	obj.rotation.z = angleZ * Math.PI / 180;
 }
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
